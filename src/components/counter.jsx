aa6ef8a877e5292ccb8b8f846aa48d6cd5869e4c
chayadeaver <chayadeaver@gmail.com>
2020-06-01 23:01:32 -0400

    passing children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,7 +11,7 @@ class Counter extends Component {
 
 
     render() { 
-        
+
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
